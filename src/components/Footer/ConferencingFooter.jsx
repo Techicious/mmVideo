b6ef8a2a0b536f0320a3873a24bccd64ae8fdce3
Umesh Kumar <39588540+Hon3y9718@users.noreply.github.com>
2024-05-29 16:58:58 +0530

    Update ConferencingFooter.jsx

--- src/components/Footer/ConferencingFooter.jsx
+++ src/components/Footer/ConferencingFooter.jsx
@@ -31,7 +31,7 @@ export const ConferencingFooter = () => {
         {/* <ToggleWidgets /> */}
         <Playlist type={HMSPlaylistType.audio} />
         <Playlist type={HMSPlaylistType.video} />
-        <PIP />
+        isMobile && <PIP />
         {/* <VirtualBackground /> */}
         {FeatureFlags.enableTranscription ? <TranscriptionButton /> : null}
         <Flex
@@ -46,13 +46,13 @@ export const ConferencingFooter = () => {
         >
           {/* {isMobile && <EmojiReaction />} */}
           {/* <MetaActions isMobile /> */}
-          <PIP />
+          isMobile && <PIP />
         </Flex>
       </AppFooter.Left>
       <AppFooter.Center>
         <AudioVideoToggle />
         <ScreenshareToggle />
-        <PIP />
+        isMobile && <PIP />
         {/* <MoreSettings /> */}
         <Flex
           align="center"
@@ -66,7 +66,7 @@ export const ConferencingFooter = () => {
         {/* {!isMobile && <EmojiReaction />} */}
         {/* <MetaActions /> */}
         <ChatToggle />
-        <PIP />
+        isMobile && <PIP />
       </AppFooter.Right>
     </AppFooter.Root>
   );
